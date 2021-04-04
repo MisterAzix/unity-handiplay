1580d23ed5d4f973af23aa6a5e97d54f7265509b
LeFlan <clement.renou@mmibordeaux.com>
2021-04-05 01:36:33 +0200

    Debut J'arrive

---
+++ Assets/Easter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Easter : MonoBehaviour
+{
+    public bool EasterToggle;
+        public void Toggle(){
+        EasterToggle = !EasterToggle;
+        Debug.Log(EasterToggle);
+    }
+}
