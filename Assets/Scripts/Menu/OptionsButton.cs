b4b872d2b2e2bc4e23f259acc6cbbb3c763af6ef
LeFlan <clement.renou@mmibordeaux.com>
2021-04-04 22:05:30 +0200

    bouton pause

--- Assets/Scripts/Menu/OptionsButton.cs
+++ Assets/Scripts/Menu/OptionsButton.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
+// using UnityEngine.SceneManagement;
 
-public class OptionsButton : EventTrigger
-{
-   public override void OnPointerClick(PointerEventData data)
-    {
-          SceneManager.LoadScene("Options");
-    }
+// public class OptionsButton : EventTrigger
+// {
+//    public override void OnPointerClick(PointerEventData data)
+//     {
+//           SceneManager.LoadScene("Options");
+//     }
     
+// }
+ bool IsActive = false;
+
+public class OptionsButton : MonoBehaviour
+{
+
+      void Update()
+      { 
+           if (Input.GetButton("Cancel"))
+            {    
+                  isActive = !isActive;
+                  GameObject.SetActive(isActive);
+            }
+      }
 }
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehavior : MonoBehaviour
 {
@@ -48,6 +49,10 @@ public class PlayerBehavior : MonoBehaviour
         }
 
         rb2D.MovePosition(newPos);
+        if (Input.GetButton("Cancel"))
+        {
+            SceneManager.LoadScene("Options");
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
