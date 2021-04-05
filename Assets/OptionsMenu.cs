5d659905ecab6fa74c34f5710ff99696c6803b5e
LeFlan <clement.renou@mmibordeaux.com>
2021-04-05 18:01:16 +0200

    fix bind options

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{     
+    public GameObject pause;
+    public GameObject audio;
+    public bool isActive;
+    void Update(){
+        if (Input.GetButtonUp("Cancel"))
+        {   
+       Invert();
+        }  
+      if (isActive)
+      {
+        pause.SetActive(true);
+        audio.SetActive(false);
+
+      }
+      if (isActive==false)
+      {
+        pause.SetActive(false);
+        audio.SetActive(true);
+      }
+
+    }
+    public void Invert(){
+ isActive = !isActive;
+    }
+}
---
+++ Assets/ReturnMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnMenu : MonoBehaviour
+{
+    public GameObject go;
+    public void Invert(){
+        go.GetComponent<OptionsMenu>().Invert();
+    }
+}
