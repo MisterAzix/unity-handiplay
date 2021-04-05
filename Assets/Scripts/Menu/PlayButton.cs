05e63b830c4c0a0bf53f1a215239657dcebee9bb
LeFlan <clement.renou@mmibordeaux.com>
2021-04-05 02:08:22 +0200

    touche enter du menu + musique intro

--- Assets/Scripts/Menu/PlayButton.cs
+++ Assets/Scripts/Menu/PlayButton.cs
@@ -2,11 +2,19 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class PlayButton : EventTrigger
-{
-   public override void OnPointerClick(PointerEventData data)
-    {
-        SceneManager.LoadScene("Level_1");
-    }
+public class PlayButton : MonoBehaviour {
     
+//    public override void OnPointerClick(PointerEventData data)
+//     {
+//         SceneManager.LoadScene("Level_1");
+//     }
+ public void Load(){
+      SceneManager.LoadScene("Level_1");
+ }
+void Update() {
+     if (Input.GetButtonUp("Fire1"))
+     {
+         Load();
+     }
+ }
 }
